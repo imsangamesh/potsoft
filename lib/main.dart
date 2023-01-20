3a18368210290dce23e32c07f1cb4719da2f0041
crackheadltd <crackheadltd@gmail.com>
2023-01-20 21:38:00 +0530

    titlechange

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Potsoft',
       debugShowCheckedModeBanner: false,
       themeMode: globalThemeContrl.themeMode,
       theme: MyThemes.lightTheme,
